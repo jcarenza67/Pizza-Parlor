01b40c00290c78bd9af21f6cfc5c15fef5bffc3d
Joseph Wilfong <josephwilfong91@gmail.com>
2023-04-07 15:25:36 -0700

    WIP I think its the toppings being an array thats messing things up, im going to just do specialty pizzas instead of multiselect toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 // Business Logic for Pizza Types
-function Pizza (toppings = [], crust = "", size = "", toppingPrices = {}) {
-  this.toppings = toppings;
+function Pizza (specials = "", crust = "", size = "", toppingPrices = {}) {
+  this.specials = specials;
   this.size = size;
   this.crust = crust;
   this.toppingPrices = toppingPrices;
   this.basePrice = 0;
 }
 
-const toppings = {
-  "pepperoni": 1.75,
-  "sausage": 2,
-  "canadian bacon": 1.50,
-  "green peppers": 0.50,
-  "artichokes": 0.50,
-  "mushrooms": 0.50
+const specialtyToppings = {
+  "Pepperoni lovers": 10.75,
+  "Margherita": 9.99,
+  "Vegetarian": 11.50,
+  "Meat lovers": 13.50,
+  "Hearty artichoke": 10.50,
+  "Shroom pie": 9.50
 };
 
 const crusts = {
