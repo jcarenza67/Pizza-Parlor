9378fc263a48318c64e3ba71c8b653dc7f757ba3
Joseph Wilfong <josephwilfong91@gmail.com>
2023-04-11 20:43:10 -0700

    fix where parameter objects had variable creations, remove this.basePrice because it was pointless, fix where variables were global and are now scoped

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,30 @@
-function Pizza (specials = "", crust = "", size = "", toppingPrices = {}) {
-  this.specials = specials;
-  this.size = size;
-  this.crust = crust;
-  this.toppingPrices = toppingPrices;
-  this.basePrice = 0;
+function Pizza (toppings, crust, size, toppingPrices, crusts, sizes) {
+  this.toppings = toppings || [];
+  this.size = size || "";
+  this.crust = crust || "";
+  this.toppingPrices = toppingPrices || {};
+  this.crusts = crusts || {};
+  this.sizes = sizes || {};
 }
 
-const specialtyToppings = {
-  "Pepperoni lovers": 10.75,
-  "Margherita": 9.99,
-  "Vegetarian": 11.50,
-  "Meat lovers": 13.50,
-  "Hearty artichoke": 10.50,
-  "Shroom pie": 9.50
-};
-
-const crusts = {
-  "Thin and Crispy": 5,
-  "Original": 7,
-  "Hand Tossed": 8.25,
-  "Deep Dish": 10
-};
-
-const sizes = {
-  "Personal": 7,
-  "Medium": 9,
-  "Large": 10.50,
-  "Xtra Large": 12
-
-};
-
 Pizza.prototype.getPrice = function() {
-  let totalPrice = this.basePrice;
-  
-  if (crusts.hasOwnProperty(this.crust)) {
-    totalPrice += crusts[this.crust];
+  let totalPrice = 0;
+
+  if (this.crust in this.crusts) {
+    totalPrice += this.crusts[this.crust];
   }
   
-  if (sizes.hasOwnProperty(this.size)) {
-    totalPrice += sizes[this.size];
+  if (this.size in this.sizes) {
+    totalPrice += this.sizes[this.size];
   }
     
-  if (specialtyToppings.hasOwnProperty(this.specials)) {
-    totalPrice += specialtyToppings[this.specials];
+  if (this.toppings.length > 0) {
+    for (let i = 0; i < this.toppings.length; i++) {
+      const topping = this.toppings[i];
+      if (this.toppingPrices.hasOwnProperty(topping)) {
+        totalPrice += this.toppingPrices[topping];
+      }
+    }
   }
   
   return totalPrice;
@@ -56,7 +38,7 @@ Pizza.prototype.getPrice = function() {
 window.addEventListener("load", function(){
   const crustSelect = document.getElementById("crust-select");
   const sizeSelect = document.getElementById("size-select");
-  const toppingSelect = document.getElementById("topping-select");
+  const toppingCheckboxes = document.querySelectorAll(".topping-checkbox");
   const cartButton = document.getElementById("cart");
   const summaryDiv = document.getElementById("summary");
   const pizzaCrust = document.getElementById("crust-summary");
@@ -66,21 +48,55 @@ window.addEventListener("load", function(){
 
   const inCart = false;
 
+  const toppingPrices = {
+    "Pepperoni": 1.50,
+    "Mushrooms": 1.00,
+    "Olives": 1.25,
+    "Onions": 0.75,
+    "Peppers": 0.50,
+    "Anchovies": 2.00
+  };
+  
+  const crusts = {
+    "Thin and Crispy": 5,
+    "Original": 7,
+    "Hand Tossed": 8.25,
+    "Deep Dish": 10
+  };
+  
+  const sizes = {
+    "Personal": 7,
+    "Medium": 9,
+    "Large": 10.50,
+    "Xtra Large": 12
+  
+  };
+
   function addToCart(event){
     event.preventDefault();
 
-
-
     const selectedCrust = crustSelect.value;
     const selectedSize = sizeSelect.value;
-    const selectedTopping = toppingSelect.value;
     
+    if(selectedCrust === "" || selectedSize === "") {
+      alert("Please select a crust and size");
+      return;
+    }
 
-    const pizza = new Pizza(selectedTopping, selectedCrust, selectedSize, specialtyToppings); 
+    const selectedToppings = [];
+
+    for (let i = 0; i < toppingCheckboxes.length; i++) {
+      const checkbox = toppingCheckboxes[i];
+      if (checkbox.checked) {
+        selectedToppings.push(checkbox.value);
+      }
+    }
+  
+    const pizza = new Pizza(selectedToppings, selectedCrust, selectedSize, toppingPrices, crusts, sizes); 
 
     pizzaCrust.textContent = "Crust: " + pizza.crust;
     pizzaSize.textContent = "Size: " + pizza.size;
-    pizzaTopping.textContent = "Selected Specialty Pizza: " + pizza.specials;
+    pizzaTopping.textContent = "Selected Toppings: " + pizza.toppings.join();
     totalPrice.textContent = "Total: $" + pizza.getPrice().toFixed(2);
 
     summaryDiv.classList.remove("hidden");
